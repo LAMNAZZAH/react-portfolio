f38af3fd1c38637ce3869e03d6e44f2173cf60cd
LAMNAZZAH <medlmz2016@gmail.com>
2022-04-14 02:47:09 +0000

    about component edit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,10 +7,14 @@ function about() {
       <div className="a-left">
         <div className="a-card bg"></div>
         <div className="a-card">
-          <img src="" alt="" className="a-img" />
+          <img src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="a-img" />
         </div>
       </div>
-      <div className="a-right">right</div>
+      <div className="a-right">
+          <h1 className="a-title">About me</h1>
+          <p className="a-sub">I'm a fulltime freelancer developper very passionate about what I do <br /> in the field since 2018</p>
+          <p className="a-desc">I had my highschool degree in 2019, I joined <br /> the superior school of technologies <br /> In 2020 and since then i realized how much I love DEV!</p>
+      </div>
     </div>
   );
 }
